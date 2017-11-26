3e8066a9bb726ccb8eca8aba53913fe4573351de
lomot coyote <lomot@lomot-Mac.local>
2017-11-26 15:25:01 +0800

    unix problem

---
+++ unix/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+echo "installing UESTC-NetworkAutoConnect"
+#
+# 检查是否存在 /etc/connect
+if [ ! -d "/etc/connect" ]; then
+  mkdir /etc/connect
+else
+	echo "0"
+fi
+#
+cp ./* /etc/connect
+chmod +x /etc/connect/*
+#
+# add task to crontab
+echo "*/1 * * * * /bin/sh /etc/connect/autoconnect.sh" >> /etc/crontabs/root
+echo "0 19 * * 1 echo '' > /etc/connect/connection.log" >> /etc/crontabs/root
+#
+# enable cron
+/etc/init.d/cron enable
+/etc/init.d/cron start
