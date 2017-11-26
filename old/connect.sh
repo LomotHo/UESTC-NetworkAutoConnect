82f98b5324e4eb9870c67599b535ff813377937f
lomot coyote <lomot@lomot-Mac.local>
2017-11-26 15:34:02 +0800

    unix problem

--- old/connect.sh
+++
@@ -1,1 +0,0 @@
-curl 'http://192.168.9.8/include/auth_action.php' -H ' xxxxxxxxxxxxxx
--- unix/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sed -i "/\/etc\/connect\/autoconnect.sh/d" /etc/crontabs/root
-sed -i "/\/etc\/connect\/connection.log/d" /etc/crontabs/root
-rm -rf /etc/connect
