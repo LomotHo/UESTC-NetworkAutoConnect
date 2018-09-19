3ffe220a95f84dfc1a126bc2a372c374aa4971f7
lomot <lomot@qq.com>
2018-09-19 09:36:32 +0800

    add date to log

--- autoconnect.sh
+++ autoconnect.sh
@@ -5,12 +5,12 @@ ping -c 1 www.baidu.com > /dev/null 2>&1
 if [ $? -eq 0 ];then  
     echo [`date`] ok1 >> /etc/connect/connection.log
 else  
-    echo bad net1 >> /etc/connect/connection.log
+    echo [`date`] bad net1 >> /etc/connect/connection.log
     ping -c 1 114.114.114.114 > /dev/null 2>&1
     if [ $? -eq 0 ];then  
         echo [`date`] ok2  >> /etc/connect/connection.log
     else  
-        echo bad net2  >> /etc/connect/connection.log
+        echo [`date`] bad net2  >> /etc/connect/connection.log
         ping -c 1 114.114.114.114 > /dev/null 2>&1  
         if [ $? -eq 0 ];then  
             echo [`date`] ok3  >> /etc/connect/connection.log
