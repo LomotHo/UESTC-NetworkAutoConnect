98204e8402da9c09e99541643f476e4f0327b3a5
lomot <lomot@qq.com>
2018-11-04 00:59:28 +0800

    use curl to check the network

--- autoconnect.sh
+++ autoconnect.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 #
 # test net connection
-ping -c 1 www.baidu.com > /dev/null 2>&1  
+# TEST_HOST=114.114.114.114
+TEST_HOST=www.baidu.com
+curl --connect-timeout 5 -m 10 $TEST_HOST > /dev/null 2>&1  
 if [ $? -eq 0 ];then  
     echo [`date`] ok1 >> /etc/connect/connection.log
 else  
     echo [`date`] bad net1 >> /etc/connect/connection.log
-    ping -c 1 114.114.114.114 > /dev/null 2>&1
+    curl --connect-timeout 5 -m 10 $TEST_HOST > /dev/null 2>&1
     if [ $? -eq 0 ];then  
         echo [`date`] ok2  >> /etc/connect/connection.log
     else  
         echo [`date`] bad net2  >> /etc/connect/connection.log
-        ping -c 1 114.114.114.114 > /dev/null 2>&1  
+        curl --connect-timeout 5 -m 10 $TEST_HOST > /dev/null 2>&1
         if [ $? -eq 0 ];then  
             echo [`date`] ok3  >> /etc/connect/connection.log
         else  
